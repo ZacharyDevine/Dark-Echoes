c696663f5248092f70bd7946bcbdafd4a9453a06
Zachary Devine <zach.devine17@gmail.com>
2025-06-06 07:14:32 -0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 //import statements
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
   // vvariables and javascript logic
+  const [listOfEpisodes, setListOfEpisodes] = useState(episodeList);
   //return(JSX and Javascript)
+
+
+  return{
+    
+  }
 }
+
